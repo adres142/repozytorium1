b1a36082a59e09ad7af5c49204afddebed544d55
Genek <adres142@onet.pl>
2021-04-01 16:26:42 +0200

    branch

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hejo");
-            Console.WriteLine("hejo");
+            Console.WriteLine("branchsddfdfs");
+           
         }
     }
 }
